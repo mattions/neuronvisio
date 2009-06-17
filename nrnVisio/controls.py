c8d7b679a21c47731eef5e374b4ec940fbcd81aa
Michele Mattioni <mattioni@ebi.ac.uk>
2009-06-17 17:56:38 +0100

    Using the CamelCase in the name everywhere

--- nrnVisio/controls.py
+++ nrnVisio/controls.py
@@ -1,9 +1,9 @@
 """
  * Copyright (C) Thu May 28 00:58:29 BST 2009 - Michele Mattioni:
  *  
- * This file is part of Neuronvisio
+ * This file is part of NeuronVisio
  * 
- * Neuronvisio is free software: you can redistribute it and/or modify
+ * NeuronVisio is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Neuronvisio.  If not, see <http://www.gnu.org/licenses/>.
+ * along with NeuronVisio.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
