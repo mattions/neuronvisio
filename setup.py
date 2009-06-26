58d880c2f5360359fa3a27a3ed874667f7ff4cdb
Michele Mattioni <mattioni@ebi.ac.uk>
2009-06-26 18:58:26 +0100

    Inserting the setup for the installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# File to install NeuronVisio module
+# To install the package run: 
+# python setup.py install
+
+
+from distutils.core import setup
+
+setup(name='NeuronVisio',
+      version='0.1',
+      description='NeuronVisio is a GTK2 user interface for NEURON simulator.',
+      author='Michele Mattioni',
+      author_email='mattioni@ebi.ac.uk',
+      url='http://mattions.github.com/neuronvisio/',
+      packages=['nrnVisio'],
+      package_data={'nrnVisio': ['examples']}
+     )
