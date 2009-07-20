1f58db2b62e73ea83a7e2f80f3c7e55f253760ef
Michele Mattioni <mattioni@ebi.ac.uk>
2009-07-20 14:28:43 +0200

    Removed a typo

--- examples/pyramidal/main.py
+++ examples/pyramidal/main.py
@@ -24,7 +24,7 @@ from neuron import h
 # loading the model
 # importing the interview so the GUI does not freeze
 # Uncomment this if you want to use also the interview GUI
-#simport neuron.gui
+#import neuron.gui
 
 # Load the script
 h.load_file("demo.hoc")
