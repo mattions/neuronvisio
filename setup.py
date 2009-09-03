cbddd6ae0cb55fdfe8042656b646f66b4186d98c
Michele Mattioni <mattioni@ebi.ac.uk>
2009-09-03 15:01:53 +0100

    Releasing 0.3.3 version.

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@
 from distutils.core import setup
 
 setup(name='NeuronVisio',
-      version='0.3.22',
-      description='NeuronVisio is a GTK2 user interface for NEURON simulator.',
+      version='0.3.3',
+      description='NeuronVisio is a GTK2 user interface for NEURON \
+      simulator.',
       author='Michele Mattioni',
       author_email='mattioni@ebi.ac.uk',
       url='http://mattions.github.com/neuronvisio/',
