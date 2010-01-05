63eaa6cc8a5bf4c4ed9f2eaa6911e6cf74525769
Michele Mattioni <mattioni@ebi.ac.uk>
2010-01-05 20:17:19 +0000

    First qt version of the UI. Signal not connected.

---
+++ neuronvisio/colorbutton.py
@@ -0,0 +1,57 @@
+"""
+colorbutton.py
+
+A tool button that enables the user to select colors using a color dialog.
+
+Copyright (C) 2007 David Boddie <david@boddie.org.uk>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class ColorButton(QToolButton):
+
+    __pyqtSignals__ = ("colorChanged(QColor)",)
+    
+    def __init__(self, parent = None):
+    
+        QToolButton.__init__(self, parent)
+        self.connect(self, SIGNAL("clicked()"), self.chooseColor)
+        self._color = QColor()
+    
+    def chooseColor(self):
+    
+        rgba, valid = QColorDialog.getRgba(self._color.rgba(), self.parentWidget())
+        if valid:
+            color = QColor.fromRgba(rgba)
+            self.setColor(color)
+    
+    def color(self):
+    
+        return self._color
+    
+    @pyqtSignature("QColor")
+    def setColor(self, color):
+    
+        if color != self._color:
+            self._color = color
+            self.emit(SIGNAL("colorChanged(QColor)"), self._color)
+            pixmap = QPixmap(16, 16)
+            pixmap.fill(color)
+            self.setIcon(QIcon(pixmap))
+    
+    color = pyqtProperty("QColor", color, setColor)
---
+++ neuronvisio/control2.py
@@ -0,0 +1,63 @@
+# * Copyright (C) Tue Jan  5 10:10:19 GMT 2010 - Michele Mattioni:
+# *  
+# * This file is part of NeuronVisio
+# * 
+# * NeuronVisio is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+#
+# * NeuronVisio is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+#
+# * You should have received a copy of the GNU General Public License
+# * along with NeuronVisio.  If not, see <http://www.gnu.org/licenses/>.
+
+#@PydevCodeAnalysisIgnoren 
+import os
+os.environ['ETS_TOOLKIT'] = 'qt4'
+
+from PyQt4 import QtGui, QtCore, uic
+
+from enthought.mayavi import mlab
+from enthought.tvtk.tools import visual
+
+from neuron import h
+
+import numpy as np
+import matplotlib
+matplotlib.use("Qt4Agg")
+matplotlib.interactive(True)
+import matplotlib.pyplot as plt
+
+class Control():
+    """Main class Neuronvisio"""
+    def __init__(self):
+        app = QtGui.QApplication.instance()
+        # Loading the UI
+        self.main_win = uic.loadUi("neuronvisio.ui")
+        self.main_win.Plot3D.connect(self.main_win.Plot3D, 
+                                     QtCore.SIGNAL('clicked()'), self.launch_mayavi)
+        self.main_win.show()
+        self.mayavi_started = False
+        
+        # Start the main event loop.
+        app.exec_()
+        
+    def launch_mayavi(self):
+        if self.mayavi_started is False:     
+            self.mayavi_fig = mlab.figure(size=(500,500))
+            # Tell visual to use this as the viewer.
+            visual.set_viewer(self.mayavi_fig)
+        self.plotCyl()
+        
+        
+    def plotCyl(self):
+        cyl = visual.Cylinder(pos=(0.,0.,0.), radius=8, length=10)
+        cyl2 = visual.Cylinder(pos=(8.,0.,0.,), axis=(0.,4.,5.), radius=4, length=13)
+        cyl3 = visual.Cylinder(pos=(-4.,0.,0.))
+     
+if __name__ == "__main__":
+    ctl = Control()
