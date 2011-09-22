ce07356bf44ca07008559cb9e92fa310fc7b672e
Uri Cohen <uri.cohen@gmail.com>
2011-09-23 00:17:24 +0300

    All names now comply with PEP8

--- src/neuronvisio/modeldb/ModelsTree.py
+++ src/neuronvisio/modeldb/ModelsTree.py
@@ -4,11 +4,11 @@ class ModelsTree:
 	_treeList = []
 	
 	def __init__(self, models):
-		names = models.getModelNames()
+		names = models.get_model_names()
 		self._treeList.append(('Recent Additions/Updates', []))
 		self._treeList.append(('Local Models', []))
 		self._treeList.append(('Marked Models', []))
 		self._treeList.append(('Models (' + str(len(names)) + ')', names))
 	
-	def getTree(self):
+	def get_tree(self):
 		return self._treeList
