a8a534061bdf8a082183c5e326907add6f30f543
Michele Mattioni <mattioni@ebi.ac.uk>
2011-10-02 12:04:01 +0100

    Adding Uri to the Authors list.

--- src/neuronvisio/__init__.py
+++ src/neuronvisio/__init__.py
@@ -20,7 +20,8 @@
 #
 #"""
 
-__authors__ = ['Michele Mattioni <mattioni@ebi.ac.uk>', 'Uri Cohen <uri.cohen@alice.nc.huji.ac.il>']
+__authors__ = ['Michele Mattioni <mattioni@ebi.ac.uk>', 
+               'Uri Cohen <uri.cohen@alice.nc.huji.ac.il>']
 __version__ = '0.6.2dev+'
 
 
