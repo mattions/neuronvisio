c5ed860433df2697ba582fe65c587bf09f7df175
Michele Mattioni <mattioni@ebi.ac.uk>
2012-02-24 08:04:03 +0000

    Introducing mocked class to build autodoc on ReadTheDoc.

--- docs/conf.py
+++ docs/conf.py
@@ -151,3 +151,28 @@ latex_use_parts = False
 
 # Extension Options
 autoclass_content='both'
+
+# Mocking class existence for ReadTheDocs autodoc
+
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['PyQt4', 'neuron', 'pyface']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
