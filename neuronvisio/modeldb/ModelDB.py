f64d0d8b761111fd79f109e1db77981812cbc697
Michele Mattioni <mattioni@ebi.ac.uk>
2012-05-07 22:01:43 +0100

    Fixing a typo on one of the logger output.

--- neuronvisio/modeldb/ModelDB.py
+++ neuronvisio/modeldb/ModelDB.py
@@ -167,7 +167,7 @@ class Model(object):
             data = f.read()
             pos = data.find('\x50\x4b\x05\x06') # End of central directory signature
             if (pos > 0):
-                logger.info("Trancating file at location " + str(pos + 22)+ ".")
+                logger.debug("Truncating file at location " + str(pos + 22)+ ".")
                 f.seek(pos + 22)                # size of 'ZIP end of central directory record'
                 f.truncate()
                 f.close()
