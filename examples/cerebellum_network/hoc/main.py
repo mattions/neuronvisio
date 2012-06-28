1edb5ebf1561223e9884b5d5a2c3f6447dede978
Michele Mattioni <mattions@gmail.com>
2012-06-28 03:35:01 +0100

    Added the network example, both in hoc and in neuroml

---
+++ examples/cerebellum_network/hoc/main.py
@@ -0,0 +1,30 @@
+# This example comes with NeuronVisio.
+
+# The model is the ported one from the NeuroML website
+# http://www.neuroconstruct.org/samples/index.html#Ex6_CerebellumDemo-N101EA
+
+# You need to compile the hoc file before launching it
+# $ nrnivmodl 
+
+# Importing Neuronvisio
+from neuronvisio.controls import Controls
+controls = Controls()
+
+# loading the model
+# importing the interview so the GUI does not freeze
+# Uncomment this if you want to use also the interview GUI
+#import neuron.gui
+
+# Load the script
+controls.load("Ex6_CerebellumDemo.hoc")
+
+# Uncomment, or copy paste in the ipython console to record and run our vectors
+
+# Record the voltage for all the sections
+#controls.manager.add_all_vecRef('v')
+
+# Initializing also our vectors
+#controls.init()
+
+# Run
+#controls.run()
