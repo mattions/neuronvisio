27d1f394bec65f2252d5eef5eb9f728385ce0135
Michele Mattioni <mattions@gmail.com>
2015-10-01 12:49:39 +0100

    Version bumped to 0.9.1

--- neuronvisio/__init__.py
+++ neuronvisio/__init__.py
@@ -22,7 +22,7 @@
 
 __authors__ = ['Michele Mattioni', 'Uri Cohen']
 __authors_emails__ = ['<mattioni@ebi.ac.uk>', '<uri.cohen@alice.nc.huji.ac.il>']
-__version__ = '0.9.1.dev0'
+__version__ = '0.9.1'
 
 
 import os
